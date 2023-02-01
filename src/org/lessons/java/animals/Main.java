650990a55b3c932710f4816066cb5212ee0918f4
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-01 14:36:51 +0100

    Definizione main e uso di alcune classi

---
+++ src/org/lessons/java/animals/Main.java
@@ -0,0 +1,35 @@
+package org.lessons.java.animals;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.print("+++++++++++++++++++++++++++++");
+		System.out.print("- Benvenuto allo zoo! -");
+		System.out.print("+++++++++++++++++++++++++++++");
+
+		System.out.println();
+		System.out.println();
+
+		Aquila eagle = new Aquila();
+
+		Delfino dolphin = new Delfino();
+
+		Cane dog = new Cane();
+
+		System.out.println("Ecco un'aquila che va a caccia! Si nutre di: ");
+		eagle.mangia();
+
+		System.out.println();
+
+		System.out.println("Ecco un delfino che dorme!");
+		dolphin.dormi();
+
+		System.out.println();
+
+		System.out.println("Che baccano, questo cane non smette di abbaiare!");
+		dog.verso();
+
+	}
+
+}

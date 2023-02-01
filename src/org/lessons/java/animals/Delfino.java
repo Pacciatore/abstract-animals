902e69a4c634a4b7723fa6f9d2d4aa16b472edbf
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-01 14:30:41 +0100

    Creazione sottoclasse Delfino

---
+++ src/org/lessons/java/animals/Delfino.java
@@ -0,0 +1,19 @@
+package org.lessons.java.animals;
+
+public class Delfino extends Animale {
+
+	public Delfino() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void verso() {
+		System.out.println("Crrr crrr!");
+	}
+
+	@Override
+	public void mangia() {
+		System.out.println("Pesce");
+	}
+
+}

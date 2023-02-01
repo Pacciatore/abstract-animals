325bbb8f1d695969384d977e7244b6f8b8d89cce
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-01 14:24:14 +0100

    Creazione sottoclasse Cane

---
+++ src/org/lessons/java/animals/Cane.java
@@ -0,0 +1,19 @@
+package org.lessons.java.animals;
+
+public class Cane extends Animale {
+
+	public Cane() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void verso() {
+		System.out.println("Wof wof!");
+	}
+
+	@Override
+	public void mangia() {
+		System.out.println("Carne");
+	}
+
+}

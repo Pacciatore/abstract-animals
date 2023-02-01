80e26dd81f4ca4426324d0a5f4de5fd9e79d4ec7
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-01 14:25:20 +0100

    Creazione sottoclasse Passerotto

---
+++ src/org/lessons/java/animals/Passerotto.java
@@ -0,0 +1,19 @@
+package org.lessons.java.animals;
+
+public class Passerotto extends Animale {
+
+	public Passerotto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void verso() {
+		System.out.println("Cip cip!");
+	}
+
+	@Override
+	public void mangia() {
+		System.out.println("Vermi, insetti, semi");
+	}
+
+}

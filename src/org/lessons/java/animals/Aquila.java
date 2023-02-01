0ce84233aea610d683ddf1d6cfc01158fe66f1db
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-01 14:26:54 +0100

    Creazione sottoclasse Aquila

---
+++ src/org/lessons/java/animals/Aquila.java
@@ -0,0 +1,19 @@
+package org.lessons.java.animals;
+
+public class Aquila extends Animale {
+
+	public Aquila() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void verso() {
+		System.out.println("Screech!");
+	}
+
+	@Override
+	public void mangia() {
+		System.out.println("Carne");
+	}
+
+}
